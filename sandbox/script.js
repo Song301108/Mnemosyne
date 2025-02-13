f490756f26f1a00f036a4e1475605137c83fbf1a
FIC <fic@info.local>
2025-02-12 21:53:08 -0300

    JavaScript

---
+++ sandbox/script.js
@@ -0,0 +1,50 @@
+console.log("Hello World!");
+
+var idade = 16;
+var idade = "Dezesseis";
+console.log(idade);
+
+let nome = "Laura";
+nome = "Angelica";
+console.log(nome);
+
+const cargo = "Aluna";
+console.log(cargo);
+
+// Exercício 3
+const nome_ = "Laura";
+let idade_ = 16;
+
+let mensagem = `Meu nome é ${nome_}, tenho ${idade_} anos.`;
+
+console.log(mensagem);
+
+// Tipos primitivos
+console.log(typeof nome_);
+console.log(typeof idade_);
+
+let menorDeIdade = true;
+let temfilhos = false;
+
+let tamanho;
+console.log(tamanho);
+
+let vazio = null;
+console.log(vazio);
+
+// Exercício 3 - Tipos primitivos
+let a = 10;
+let b = "Laura";
+let c = "10";
+let d = 20;
+
+console.log("a + b", a + b);
+console.log("a + c", a + c);
+console.log("a + d", a + d);
+
+let n1 = "42.5";
+n1 = Number(n1);
+console.log(n1);
+
+console.log(parseFloat("40.12Ae"));
+console.log("40.12Ae");
