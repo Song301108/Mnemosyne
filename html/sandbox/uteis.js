058a363fb2d8253e695c64fe017b0027b5bb41df
FIC <fic@info.local>
2025-02-20 21:51:33 -0300

    site em desenvolvimento

---
+++ html/sandbox/uteis.js
@@ -0,0 +1,64 @@
+// const numeros = [1, 2, 3, 4, 5, 6]
+
+// const numerosMaioresQueDois = numeros.filter(numero => numero > 2)
+
+// BÁSICO - FILTER
+// EXERCÍCIO 1
+// const numeros = [5, 8, 12, 15, 3, 20, 7];
+// const maioresQueDez = numeros.filter(numero => numero > 10);
+// console.log(maioresQueDez);
+
+// EXERCÍCIO 2
+// const nomes = ["Ana", "João", "Amanda", "Carlos", "Arthur", "Beatriz"];
+// const nomesComA = nomes.filter(nome => nome.startsWith("A"));
+// console.log(nomesComA);
+
+// INTERMEDIÁRIO
+// EXERCÍCIO 1
+// const produtos = [
+//     { nome: "Teclado", preco: 120 },
+//     { nome: "Mouse", preco: 40 },
+//     { nome: "Fone de ouvido", preco: 80 },
+//     { nome: "Caderno", preco: 30 }
+//   ];
+//   const produtosBaratos = produtos.filter(produto => produto.preco < 50);
+//   console.log(produtosBaratos);
+  
+// BÁSICO - FIND
+// EXERCÍCIO 1
+// const nomes = ["João", "Carlos", "Amanda", "Arthur", "Beatriz"];
+// const amanda = nomes.find(nome => nome == "Amanda");
+// console.log(amanda);
+
+// EXERCÍCIO 2
+// const listaDeUsuarios = [
+//     { id: 1, nome: "Alice" },
+//     { id: 2, nome: "Bruno" },
+//     { id: 3, nome: "Carla" }
+//   ];
+  
+//   const usuarioEncontrado = listaDeUsuarios.find(usuario => usuario.id == 2);
+  
+//   console.log(usuarioEncontrado); 
+
+// INTERMEDIÁRIO - FIND
+// EXERCÍCIO 1
+// const produtos = [
+//     { nome: "Teclado", preco: 120 },
+//     { nome: "Mouse", preco: 40 },
+//     { nome: "Fone de ouvido", preco: 80 },
+//     { nome: "Caderno", preco: 30 }
+//   ];
+//   const produtoBuscado = 
+//   console.log(produtoBarato); 
+  
+// BÁSICO - MAP
+// EXERCÍCIO 1 
+// const palavras = ["olá", "mundo", "javascript"];
+// const enfatizadas = palavras.map(palavra => palavra + "!!!");
+// console.log(enfatizadas);
+
+// EXERCÍCIO 2
+// const palavras = ["olá", "mundo", "javascript"];
+// const enfatizadas = palavras.map(palavra => palavra + "!!!");
+// console.log(enfatizadas);
